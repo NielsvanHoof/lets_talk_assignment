abcf707fd2ed4f495f5a9712cd5533f9340ad8d5
Niels van Hoof <vanhoofniels15@gmail.com>
2025-01-11 15:39:49 +0100

    refactor IP check middleware to work for the dashboard routes only (allow access to ip, user managment for now)

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\DashBoardController;
 use App\Http\Controllers\IpAddressController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\EnsureIpIsAllowed;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -11,13 +12,15 @@ Route::get('/', function () {
     return Inertia::render('Welcome');
 });
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth', EnsureIpIsAllowed::class])->group(function () {
     Route::get('/dashboard', [DashBoardController::class, 'index'])->name('dashboard');
     Route::post('/exchange-rates/update', [DashBoardController::class, 'update'])->name('exchange-rates.update');
     Route::post('/exchange-rates/schedule', [DashBoardController::class, 'schedule'])->name('exchange-rates.schedule');
     Route::post('/exchange-rates/{pipeline}/enable', [DashBoardController::class, 'enable'])->name('exchange-rates.enable');
     Route::delete('/exchange-rates/{pipeline}/disable', [DashBoardController::class, 'disable'])->name('exchange-rates.disable');
+});
 
+Route::middleware('auth')->group(function () {
     Route::get('/ip-addresses', [IpAddressController::class, 'index'])->name('ip-addresses.index');
     Route::post('/ip-addresses', [IpAddressController::class, 'store'])->name('ip-addresses.store');
     Route::delete('/ip-addresses/{ipAddress}', [IpAddressController::class, 'destroy'])->name('ip-addresses.destroy');
